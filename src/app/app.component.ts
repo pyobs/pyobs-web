befaca619462430ba546c413f1db415bbbc3ea68
Tim-Oliver Husser <thusser@uni-goettingen.de>
2018-05-12 20:25:07 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pytel-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pytel';
+}
