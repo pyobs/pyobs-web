aacb3f0d8b40c62b3b6df2f51d58b3393b772d63
Tim-Oliver Husser <thusser@uni-goettingen.de>
2018-05-29 12:10:41 +0200

    basic image search works

--- src/app/imagedb/list-images/list-images.component.ts
+++ src/app/imagedb/list-images/list-images.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChange} from '@angular/core';
 import {Observable} from 'rxjs';
 import {IImageDbService} from '../../shared/json-rpc.service';
 import {faImage, faDownload} from '@fortawesome/free-solid-svg-icons';
@@ -8,11 +8,10 @@ import {faImage, faDownload} from '@fortawesome/free-solid-svg-icons';
     templateUrl: './list-images.component.html',
     styleUrls: ['./list-images.component.css']
 })
-export class ListImagesComponent implements OnInit {
+export class ListImagesComponent implements OnInit, OnChanges {
     // inputs
     @Input() module: string;
-    @Input() observation: string;
-    @Input() reduction_status: number;
+    @Input() query = {};
     @Input() itemsPerPage = 50;
 
     // font awesome icons
@@ -29,15 +28,19 @@ export class ListImagesComponent implements OnInit {
     }
 
     ngOnInit() {
-        // get image count
-        this.count$ = this.IImageDb.count_images(this.module,
-            {observation: this.observation, reduction_status: this.reduction_status, include_details: true});
-
         // initial update
+        this.updateCount();
         this.updateList();
     }
 
-    public pageChanged($event) {
+    ngOnChanges(changes: {[propertyName: string]: SimpleChange}) {
+        if (changes['query'] && this.query) {
+            this.updateCount();
+            this.updateList();
+        }
+    }
+
+    pageChanged($event) {
         // set current page
         this.currentPage = $event.page - 1;
 
@@ -45,15 +48,27 @@ export class ListImagesComponent implements OnInit {
         this.updateList();
     }
 
-    public updateList() {
+    updateCount() {
+        // prepare params
+        const params = this.query;
+        params['include_details'] = true;
+
+        // get image count
+        this.count$ = this.IImageDb.count_images(this.module, params);
+    }
+
+    updateList() {
         // reset list of images, so that the list can show a load animation
         this.images = null;
 
+        // prepare params
+        const params = this.query;
+        params['include_details'] = true;
+        params['offset'] = this.currentPage * this.itemsPerPage;
+        params['limit'] = this.itemsPerPage;
+
         // load new list of images
-        this.IImageDb.find_images(this.module, {
-            observation: this.observation, reduction_status: this.reduction_status,
-            include_details: true, offset: this.currentPage * this.itemsPerPage, limit: this.itemsPerPage
-        }).subscribe(data => this.images = data);
+        this.IImageDb.find_images(this.module, params).subscribe(data => this.images = data);
     }
 
 }
--- src/app/imagedb/search-images/search-images.component.ts
+++ src/app/imagedb/search-images/search-images.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {
     faImages, faUser, faMoon, faCamera, faTasks, faFolderOpen, faGlobe, faClock, faFilter, faTh, faStar,
-    faMapMarker, faCircle
+    faMapMarker, faCircle, faCalendar
 } from '@fortawesome/free-solid-svg-icons';
 import {IImageDbService} from '../../shared/json-rpc.service';
 import {Observable} from 'rxjs';
@@ -33,13 +33,14 @@ export class SearchImagesComponent implements OnInit {
     faStar = faStar;
     faMapMarker = faMapMarker;
     faCircle = faCircle;
+    faCalendar = faCalendar;
 
     // list of telescopes and instruments
     telescopes$: Observable<any>;
     instruments$: Observable<any>;
 
     // ngModel variable for image type
-    imageType = 'light';
+    imageType = 'object';
     reductionStatus = 'reduced';
 
     constructor(private route: ActivatedRoute, private IImageDb: IImageDbService) {
@@ -68,14 +69,18 @@ export class SearchImagesComponent implements OnInit {
         if (form.value.observation.length > 0) {
             query['name'] = form.value.observation;
         }
-        if (form.value.nights !== null) {
+        if (form.value.date !== null) {
             const pipe = new DatePipe('en-US');
-            query['night_start'] = pipe.transform(form.value.nights[0], 'yyyy-MM-dd');
-            query['night_end'] = pipe.transform(form.value.nights[1], 'yyyy-MM-dd');
+            query['date_start'] = pipe.transform(form.value.date[0], 'yyyy-MM-dd');
+            query['date_end'] = pipe.transform(form.value.date[1], 'yyyy-MM-dd');
         }
         if (form.value.task.length > 0) {
             query['task'] = form.value.task;
         }
+
+        // image type and reduction status
+        query['image_type'] = form.value.imageType;
+        query['reduction_status'] = form.value.reductionStatus === 'reduced' ? 1 : 0;
 
         // set it
         this.query = query;
